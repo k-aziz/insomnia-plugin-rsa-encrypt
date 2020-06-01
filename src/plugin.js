90c2db49e0d84f7b8c051443237dd151ab80f951
Francesco Milani <fmilani@bink.com>
2020-06-01 16:37:27 +0300

    use String instead of JSON.Stringify

--- src/plugin.js
+++ src/plugin.js
@@ -1,57 +1,55 @@
 var forge = require('node-forge');
 
 module.exports.templateTags = [{
-  name: 'RsaEncrypt',
-  displayName: 'RSA encrypt',
-  description: 'Encrypt value with RSA OAEP',
-  args: [
-      {
-          displayName: 'Public RSA key',
-          type: 'string',
-          defaultValue: '',
-      },
-      {
-          displayName: 'Value',
-          type: 'string',
-          defaultValue: '',
-      },
-    {
-        displayName: 'Function',
-        type: 'enum',
-        options: [
-            {
-                displayName: 'Encrypt',
-                value: 'ENCRYPT'
-            },
-            {
-                displayName: 'Decrypt',
-                value: 'DECRYPT'
-            }
+    name: 'RsaEncrypt',
+    displayName: 'RSA encrypt',
+    description: 'Encrypt value with RSA OAEP',
+    args: [{
+            displayName: 'Public RSA key',
+            type: 'string',
+            defaultValue: '',
+        },
+        {
+            displayName: 'Value',
+            type: 'string',
+            defaultValue: '',
+        },
+        {
+            displayName: 'Function',
+            type: 'enum',
+            options: [{
+                    displayName: 'Encrypt',
+                    value: 'ENCRYPT'
+                },
+                {
+                    displayName: 'Decrypt',
+                    value: 'DECRYPT'
+                }
 
-       ]
-    }
+            ]
+        }
 
-  ],
-  async run(context, key, value, fn) {
-      var val;
-      if (fn === "ENCRYPT") { 
-          console.log(`Encrypting value ${value}`)
+    ],
+    async run(context, key, value, fn) {
+        var val;
+        if (fn === "ENCRYPT") {
+            console.log(`Encrypting value ${value}`);
 
-          var publicKey = forge.pki.publicKeyFromPem(key);
-          var buf = forge.util.createBuffer(value, 'utf8');
+            var publicKey = forge.pki.publicKeyFromPem(key);
+            var buf = forge.util.createBuffer(value, 'utf8');
 
-          var encrypted = publicKey.encrypt(buf.bytes(), 'RSAES-OAEP');
-          var b64 = forge.util.encode64(encrypted);
+            var encrypted = publicKey.encrypt(buf.bytes(), 'RSAES-OAEP');
+            var b64 = forge.util.encode64(encrypted);
 
-          console.log(`Base64 encrypted value - ${b64}`)
-          val = JSON.stringify(b64)
-      } else if (fn === "DECRYPT") {
-          var raw_val = forge.util.decode64(value);
-          var privateKey = forge.pki.privateKeyFromPem(key);
-          var decrypted = privateKey.decrypt(raw_val, 'RSA-OAEP');
+            console.log(`Base64 encrypted value - ${b64}`);
+            val = String(b64);
+        } else if (fn === "DECRYPT") {
+            var raw_val = forge.util.decode64(value);
+            var privateKey = forge.pki.privateKeyFromPem(key);
+            var decrypted = privateKey.decrypt(raw_val, 'RSA-OAEP');
 
-          val = decrypted;
-      }
-      return val
-  },
-}];
+            val = decrypted;
+        }
+        return String(val);
+    },
+}]
