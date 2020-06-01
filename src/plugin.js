ac3087d7c9962ed6810c50524d7372e28955521a
kaziz <kaziz@bink.com>
2020-06-01 14:18:29 +0100

    Add RSA-OAEP decryption

--- src/plugin.js
+++ src/plugin.js
@@ -5,30 +5,53 @@ module.exports.templateTags = [{
   displayName: 'RSA encrypt',
   description: 'Encrypt value with RSA OAEP',
   args: [
-    {
-      displayName: 'Public RSA key',
-      type: 'string',
-      defaultValue: '',
-    },
-    {
-        displayName: 'Value',
-        type: 'string',
-        defaultValue: '',
+      {
+          displayName: 'Public RSA key',
+          type: 'string',
+          defaultValue: '',
       },
+      {
+          displayName: 'Value',
+          type: 'string',
+          defaultValue: '',
+      },
+    {
+        displayName: 'Function',
+        type: 'enum',
+        options: [
+            {
+                displayName: 'Encrypt',
+                value: 'ENCRYPT'
+            },
+            {
+                displayName: 'Decrypt',
+                value: 'DECRYPT'
+            }
+
+       ]
+    }
 
   ],
-  async run(context, key, value) {
-        console.log(`Encrypting value ${value}`)
+  async run(context, key, value, fn) {
+      var val;
+      if (fn === "ENCRYPT") { 
+          console.log(`Encrypting value ${value}`)
 
-        var publicKey = forge.pki.publicKeyFromPem(key);
-        var buf = forge.util.createBuffer(value, 'utf8');
+          var publicKey = forge.pki.publicKeyFromPem(key);
+          var buf = forge.util.createBuffer(value, 'utf8');
 
-        var encrypted = publicKey.encrypt(buf.bytes(), 'RSAES-OAEP');
-        var b64 = forge.util.encode64(encrypted);
+          var encrypted = publicKey.encrypt(buf.bytes(), 'RSAES-OAEP');
+          var b64 = forge.util.encode64(encrypted);
 
-        console.log(`Base64 encrypted value - ${b64}`)
+          console.log(`Base64 encrypted value - ${b64}`)
+          val = JSON.stringify(b64)
+      } else if (fn === "DECRYPT") {
+          var raw_val = forge.util.decode64(value);
+          var privateKey = forge.pki.privateKeyFromPem(key);
+          var decrypted = privateKey.decrypt(raw_val, 'RSA-OAEP');
 
-        return JSON.stringify(b64)
-    ;
+          val = decrypted;
+      }
+      return val
   },
 }];
